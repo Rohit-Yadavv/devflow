d3e0066274fd78de1a3ba0f7b0c9db2041466bd2
Rohit Yadav <rk4740779@gmail.com>
2023-11-19 22:47:31 +0530

    added homepage functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,41 @@
+import QuestionCard from "@/components/card/QuestionCard";
+import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/share/Filter";
+import NoResult from "@/components/share/NoResult";
 import LocalSearchBar from "@/components/share/search/LocalSearchBar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const question = [
+  {
+    _id: 1,
+    title: "Redux Toolkit Not Updating State as Expected",
+    tags: [
+      { _id: 1, name: "pyton" },
+      { _id: 2, name: "sql" },
+    ],
+    author:  { _id: 1, name: "rohit" ,picture:'rohit.jpg'},
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2023-02-01T12:00:00.000Z"),
+  },
+  {
+    _id: 2,
+    title: "Async/Await Function Not Handling Errors Properly",
+    tags: [{ _id: 1, name: "javascript" }],
+    author:  { _id: 1, name: "rohit" ,picture:'rohit.jpg'},
+    upvotes: 10,
+    views: 100,
+    answers: [],
+    createdAt: new Date("2023-02-01T12:00:00.000Z"),
+  },
+];
 
 export default function Home() {
   return (
     <>
-      {" "}
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
         <Link className="flex justify-end max-sm:w-full" href="/ask-question">
@@ -17,13 +46,43 @@ export default function Home() {
       </div>
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchBar
-            route="/"
-            iconPosition="left"
-            imgSrc="assets/icons/search.svg"
-            placeholder="search for questions"
-            otherClasses="flex-1"
-        />  
-        <Filter/>
+          route="/"
+          iconPosition="left"
+          imgSrc="assets/icons/search.svg"
+          placeholder="search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
+      </div>
+      <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {question.length > 0
+          ? question.map((question) => (
+          <QuestionCard 
+          key = {question._id}
+          _id = {question._id}
+          title = {question.title}
+          tags = {question.tags}
+          author = {question.author}
+          upvotes = {question.upvotes}
+          views = {question.views}
+          answers = {question.answers}
+          createdAt = {question.createdAt}
+          />
+          ))
+          : <NoResult
+          title="There are no question to show"
+          desc="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved! 💡"
+          link="/ask-question"
+          linkTitle="Ask a Question"
+          />
+          }
       </div>
     </>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,64 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export const getTimeStamp = (createdAt: Date): string => {
+  // Ensure that createdAt is a Date object
+  if (!(createdAt instanceof Date)) {
+    throw new Error("Invalid date object");
+  }
+
+  // Calculate the time difference in milliseconds
+  const now = new Date();
+  const timeDifference = now.valueOf() - createdAt.valueOf();
+
+  // Define time intervals in milliseconds
+  const minute = 60 * 1000;
+  const hour = 60 * minute;
+  const day = 24 * hour;
+  const week = 7 * day;
+  const month = 30 * day;
+  const year = 365 * day;
+
+  // Determine the appropriate time unit
+  if (timeDifference < minute) {
+    const seconds = Math.floor(timeDifference / 1000);
+    return `${seconds} second${seconds !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < hour) {
+    const minutes = Math.floor(timeDifference / minute);
+    return `${minutes} minute${minutes !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < day) {
+    const hours = Math.floor(timeDifference / hour);
+    return `${hours} hour${hours !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < week) {
+    const days = Math.floor(timeDifference / day);
+    return `${days} day${days !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < month) {
+    const weeks = Math.floor(timeDifference / week);
+    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`;
+  } else if (timeDifference < year) {
+    const months = Math.floor(timeDifference / month);
+    return `${months} month${months !== 1 ? "s" : ""} ago`;
+  } else {
+    const years = Math.floor(timeDifference / year);
+    return `${years} year${years !== 1 ? "s" : ""} ago`;
+  }
+};
+
+export const formatNumber = (num: number): string => {
+  const absNum = Math.abs(num);
+
+  if (absNum < 1000) {
+    return num.toString();
+  } else if (absNum < 1e6) {
+    return (num / 1e3).toFixed(1) + 'k';
+  } else if (absNum < 1e9) {
+    return (num / 1e6).toFixed(1) + 'm';
+  } else if (absNum < 1e12) {
+    return (num / 1e9).toFixed(1) + 'b';
+  } else {
+    return (num / 1e12).toFixed(1) + 't';
+  }
+};
