cef59831e8f885e27d2e556f457e3f80cbaa6dbd
Rohit Yadav <rk4740779@gmail.com>
2023-11-22 18:16:13 +0530

    added views

--- database/question.model.ts
+++ database/question.model.ts
@@ -15,6 +15,7 @@ const QuestionsSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+  views: { type: Number, default: 0 },
   upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
   author: [{ type: Schema.Types.ObjectId, ref: "User" }],
